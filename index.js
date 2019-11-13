4634b333adbaf4aae447a0efd03d58fd02825698
Enrico Massone <enrico.massone@deltatre.it>
2019-11-13 05:41:44 +0100

    cleaned up implementation (now only the iterative add is implemented)

--- index.js
+++ index.js
@@ -18,32 +18,12 @@ class Node {
   }
 }
 
-const appendChildToLeaf = (leafNode, value) => {
-  if (!leafNode.isLeaf()) {
-    throw new Error("This function is meant to append a child to a leaf node.");
-  }
-
-  if (value <= leafNode.value) {
-    leafNode.left = new Node(value);
-  } else {
-    leafNode.right = new Node(value);
-  }
-};
-
 class Tree {
   constructor() {
     this.root = null;
   }
 
   add(value) {
-    if (this.root === null) {
-      this.root = new Node(value);
-    } else {
-      addValueToTree(this.root, value);
-    }
-  }
-
-  addIterative(value) {
     if (this.root === null) {
       this.root = new Node(value);
       return;
@@ -75,29 +55,6 @@ class Tree {
     return this.root;
   }
 }
-
-const addValueToTree = (treeRoot, value) => {
-  if (treeRoot.isLeaf()) {
-    appendChildToLeaf(treeRoot, value);
-    return;
-  }
-
-  if (value <= treeRoot.value && !treeRoot.hasLeftChild()) {
-    treeRoot.left = new Node(value);
-    return;
-  }
-
-  if (value > treeRoot.value && !treeRoot.hasRightChild()) {
-    treeRoot.right = new Node(value);
-    return;
-  }
-
-  if (value <= treeRoot.value) {
-    addValueToTree(treeRoot.left, value);
-  } else {
-    addValueToTree(treeRoot.right, value);
-  }
-};
 
 exports.Node = Node;
 exports.Tree = Tree;
